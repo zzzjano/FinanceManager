59e0f81592622cc12f4c5a00324966427b4d8573
Jan Milewczyk <milewczykjan@gmail.com>
2025-06-12 13:52:19 +0200

    rate limit for resource-server

--- resource-server/src/server.js
+++ resource-server/src/server.js
@@ -10,6 +10,14 @@ const { setupLogger } = require('./utils/logger');
 const { authMiddleware } = require('./middleware/auth');
 const routes = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
+const { rateLimit } = require('express-rate-limit')
+
+const limiter = rateLimit({
+	windowMs: 5 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 // Load environment variables
 require('dotenv').config();
@@ -24,6 +32,7 @@ const app = express();
 app.use(helmet()); // Security headers
 app.use(cors()); // Cross-origin resource sharing
 app.use(express.json()); // Parse JSON request bodies
+app.use(limiter)
 app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } })); // HTTP request logging
 
 // Connect to database
